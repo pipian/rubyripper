ffcdabff4fbddcd26972cb7dffd925127ad0c958
Bouke Woudstra <boukewoudstra@gmail.com>
2012-01-31 20:49:58 +0100

    Minimize the gtk updates when ripping a new disc

--- lib/rubyripper/rippingInfoAtStart.rb
+++ lib/rubyripper/rippingInfoAtStart.rb
@@ -24,6 +24,7 @@ class RippingInfoAtStart
     @log = log
     @tracks = trackSelection.length
     @md = disc.metadata
+    @logString = String.new
   end
 
   def show
@@ -32,30 +33,31 @@ class RippingInfoAtStart
     showEncodingPrefs()
     showDiscInfo()
     showLaunch()
+    updateInterface()
   end
 
 private
 
   def showVersion
-    @log << _("This log is created by Rubyripper, version %s\n") % [$rr_version]
-    @log << _("Website: http://code.google.com/p/rubyripper\n\n")
+    @logString << _("This log is created by Rubyripper, version %s\n") % [$rr_version]
+    @logString << _("Website: http://code.google.com/p/rubyripper\n\n")
   end
 
   def showRippingPrefs
-    @log << _("Cdrom player used to rip:\n%s\n") % [@disc.devicename]
-    @log << _("Cdrom offset used: %s\n\n") % [@prefs.offset]
-    @log << _("Ripper used: cdparanoia %s\n") % [@prefs.rippersettings]
-    @log << _("Matches required for all chunks: %s\n") % [@prefs.reqMatchesAll]
-    @log << _("Matches required for erroneous chunks: %s\n\n") % [@prefs.reqMatchesErrors]
+    @logString << _("Cdrom player used to rip:\n%s\n") % [@disc.devicename]
+    @logString << _("Cdrom offset used: %s\n\n") % [@prefs.offset]
+    @logString << _("Ripper used: cdparanoia %s\n") % [@prefs.rippersettings]
+    @logString << _("Matches required for all chunks: %s\n") % [@prefs.reqMatchesAll]
+    @logString << _("Matches required for erroneous chunks: %s\n\n") % [@prefs.reqMatchesErrors]
   end
 
   def showEncodingPrefs
-    @log << _("Codec(s) used:\n")
-    @log << _("-flac \t-> %s (%s)\n") % [@prefs.settingsFlac, version('flac')] if @prefs.flac
-    @log << _("-vorbis\t-> %s (%s)\n") % [@prefs.settingsVorbis, version('oggenc')] if @prefs.vorbis
-    @log << _("-mp3\t-> %s\n(%s\n") % [@prefs.settingsMp3, version('lame')] if @prefs.mp3
-    @log << _("-wav\n") if @prefs.wav
-    @log << _("-other\t-> %s\n") % [@prefs.settingsOther] if @prefs.other
+    @logString << _("Codec(s) used:\n")
+    @logString << _("-flac \t-> %s (%s)\n") % [@prefs.settingsFlac, version('flac')] if @prefs.flac
+    @logString << _("-vorbis\t-> %s (%s)\n") % [@prefs.settingsVorbis, version('oggenc')] if @prefs.vorbis
+    @logString << _("-mp3\t-> %s\n(%s\n") % [@prefs.settingsMp3, version('lame')] if @prefs.mp3
+    @logString << _("-wav\n") if @prefs.wav
+    @logString << _("-other\t-> %s\n") % [@prefs.settingsOther] if @prefs.other
   end
 
   def version(name)
@@ -63,19 +65,24 @@ private
   end
 
   def showDiscInfo
-    @log << _("\nDISC INFO\n")
-    @log << "\n" + _('Artist') + "\t= %s" % [@md.artist]
-    @log << "\n" + _('Album') + "\t= %s" % [@md.album]
-    @log << "\n" + _("Year") + "\t= %s" % [@md.year]
-    @log << "\n" + _("Genre") + "\t= %s" % [@md.genre]
-    @log << "\n" + _("Tracks") + "\t= %s (%s selected)\n\n" % [@disc.audiotracks, @tracks]
+    @logString << _("\nDISC INFO\n")
+    @logString << "\n" + _('Artist') + "\t= %s" % [@md.artist]
+    @logString << "\n" + _('Album') + "\t= %s" % [@md.album]
+    @logString << "\n" + _("Year") + "\t= %s" % [@md.year]
+    @logString << "\n" + _("Genre") + "\t= %s" % [@md.genre]
+    @logString << "\n" + _("Tracks") + "\t= %s (%s selected)\n\n" % [@disc.audiotracks, @tracks]
 
     (1..@disc.audiotracks).each do |track|
-      @log << "#{sprintf("%02d", track)} - #{@md.trackname(track)}\n"
+      @logString << "#{sprintf("%02d", track)} - #{@md.trackname(track)}\n"
     end
   end
 
   def showLaunch
-    @log << "\n" + _("STATUS") + "\n\n"
+    @logString << "\n" + _("STATUS") + "\n\n"
+  end
+  
+  def updateInterface
+    @log << @logString
+    @logString = nil
   end
 end
