9202e3d9210a0befed0dec67414a7b57e06e826c
Bouke Woudstra <boukewoudstra@gmail.com>
2012-03-08 20:39:37 +0100

    Small fix for the gettext crash.

--- lib/rubyripper/base.rb
+++ lib/rubyripper/base.rb
@@ -27,6 +27,11 @@ ENV['GETTEXT_PATH'] = File.join($localdir, '/data/locale')
 begin
   raise() if ENV.key?('cucumber')
   require 'gettext'
+  
+  # workaround to prevent crash for gettext
+  # source: https://github.com/rubygems/rubygems/issues/171
+  module Gem;def self.all_load_paths;[];end;end
+  
   class TestIfGetTextDoesNotCrash
     include GetText
     bindtextdomain("rubyripper")
